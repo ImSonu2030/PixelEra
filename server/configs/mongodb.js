168ee838ec8df70fbfe24d9ffba52459deba61b3
ImSonu2030 <sonumahto8981@gmail.com>
2025-08-28 22:26:35 +0530

    bug fix

--- server/configs/mongodb.js
+++ server/configs/mongodb.js
@@ -4,7 +4,7 @@ const connectDB = async () => {
   mongoose.connection.on('connected',()=>{
     console.log('mongoDB connected');
   });
-  await mongoose.connect(`${process.env.MONGODB_URL}/project 0`);
+  await mongoose.connect(`${process.env.MONGODB_URL}/pixelera`);
 };
 
 export default connectDB;
