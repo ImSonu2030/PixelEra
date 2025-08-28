56cb8197fee7a7fff4b441123c4e6802f23e39bc
ImSonu2030 <sonumahto8981@gmail.com>
2025-08-28 20:34:57 +0530

    minor change

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
-import 'dotenv/config'
-import express from 'express'
-import cors from 'cors'
-import connectDB from './configs/mongodb.js';
-import userRouter from './routes/userRoutes.js';
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+import connectDB from "./configs/mongodb.js";
+import userRouter from "./routes/userRoutes.js";
 
 const PORT = process.env.PORT || 4000;
-const app=express();
+const app = express();
 
 // Intialize Middlerwares
 app.use(cors());
@@ -13,7 +13,7 @@ app.use(express.json());
 await connectDB();
 
 // API routes
-app.get('/',(req,res)=>res.send("API Working"))
-app.use('/api/user',userRouter)
+app.get("/", (req, res) => res.send("API Working"));
+app.use("/api/user", userRouter);
 
-app.listen(PORT,()=>console.log('server working at port '+PORT))
+app.listen(PORT, () => console.log("server working at port " + PORT));
